8156055611d6300c633f0c163833e6c8507f4cd7
aayush arora <aayush113002@gmail.com>
2018-07-19 02:24:55 +0530

    (Added Angular Projection): Blog1 YDKA

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
